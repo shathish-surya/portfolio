5cf87df4db5a8bc7675b9e30709a5d168abf6bb1
shathish-surya <78249002+shathish-surya@users.noreply.github.com>
2021-08-09 11:36:12 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,11 @@ sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
 sr.reveal('.skills__img', {delay: 300})
 
+/*CERTIFICATES */
+sr.reveal('.certificates__img', {interval: 200})
+
+
+
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
 
