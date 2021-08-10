7faf2810de7c6963495d7e905161122c6bc5ba17
shathish-surya <78249002+shathish-surya@users.noreply.github.com>
2021-08-10 18:07:44 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,3 +79,8 @@ sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
+
+/*CLICK DISABLE*/
+$(document).bind("contextmenu",function(e){
+  return false;
+    });
